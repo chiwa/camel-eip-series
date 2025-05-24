3b2fb9d5b73562c68f71291c1bb0622180b36a28
chiwakantawong <chiwa.k@tcrbank.com>
2025-05-24 17:27:25 +0700

    EP1

---
+++ ep0-intro/src/main/java/com/zengcode/camel/EP1Application.java
@@ -0,0 +1,13 @@
+package com.zengcode.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EP1Application.class, args);
+	}
+
+}
